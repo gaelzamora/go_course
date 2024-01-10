6598171655d43d9b7dd19bf4fc14a1a855d32e20
gaelzamora <jgaelhz@gmail.com>
2024-01-09 22:49:38 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gaelzamora/go_course
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/gaelzamora/go_course/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
